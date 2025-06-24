7837a27cb654f5adf19c6453ee41f00e5e29287b
shrimbly <falloon.willie@gmail.com>
2025-06-24 22:40:42 +1200

    v0.2.4

--- sharp_frames/__init__.py
+++ sharp_frames/__init__.py
@@ -1,6 +1,6 @@
 """Sharp Frames - Extract, score, and select the best frames from a video, video directory, or image directory."""
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 import subprocess
 import sys
